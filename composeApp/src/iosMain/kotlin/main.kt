ee77dfeced65a4660adadd6ee30d41f98755b647
devenvoy <iambeast0069@gmail.com>
2025-07-26 00:35:13 +0530

    web app release

--- composeApp/src/iosMain/kotlin/main.kt
+++
@@ -1,7 +0,0 @@
-import androidx.compose.ui.window.ComposeUIViewController
-import io.github.terrakok.compose.ui.App
-import platform.UIKit.UIViewController
-
-fun MainViewController(): UIViewController {
-    return ComposeUIViewController { App() }
-}
--- composeApp/src/jsMain/kotlin/main.kt
+++
@@ -1,8 +0,0 @@
-import io.github.terrakok.compose.ui.App
-import org.jetbrains.skiko.wasm.onWasmReady
-
-fun main() {
-    onWasmReady {
-        BrowserViewportWindow("Compose Application Wizard") { App() }
-    }
-}
